f705639796361306701495a8efc7cf0204831b8e
zachang <zachangdawuda@gmail.com>
2018-06-28 15:42:51 +0100

    formatted stack algorithm file using proper indentations according to PEP8

--- stack.py
+++ stack.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+
 class Stack:
     """Implementation of Stack algorithm"""
 
@@ -7,16 +8,19 @@ class Stack:
         self.items = []
 
     def push(self, item):
+        """Add items to list"""
         self.items.insert(0, item)
         print("Items added:",self.items)
 
     def is_empty(self):
+        """Checks is list is empty"""
         if len(self.items) == 0:
             return True
         else:
             return False
 
     def remove(self):
+        """Deletes items from list"""
         if self.is_empty():
             return None
         else:
@@ -24,9 +28,11 @@ class Stack:
             print("Items left:",self.items)
 
     def size(self):
+        """Checks for the size of the list"""
         print("Length:", len(self.items))
 
     def peek(self):
+        """Checks for single item in a list if list not is not empty"""
         if self.is_empty():
             return None
         else:
@@ -34,15 +40,15 @@ class Stack:
 
 
 if __name__ == '__main__':
-  stack = Stack()
-  stack.push(14)
-  stack.push(5)
-  stack.push(9)
-  stack.peek()
-  stack.size()
-  stack.remove()
-  stack.remove()
-  stack.peek()
-  stack.remove()
-  stack.size()
-  stack.peek()
+    stack = Stack()
+    stack.push(14)
+    stack.push(5)
+    stack.push(9)
+    stack.peek()
+    stack.size()
+    stack.remove()
+    stack.remove()
+    stack.peek()
+    stack.remove()
+    stack.size()
+    stack.peek()
