81b61f4601b686d852338cbc3f52c6eb1a518f48
zachang <zachangdawuda@gmail.com>
2018-07-17 11:48:21 +0100

    queue algorithm implementation

---
+++ queue_first.py
@@ -0,0 +1,52 @@
+class Queue:
+    """Implementation of Stack algorithm"""
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        """Add items to list"""
+        self.items.append(item)
+        print("Items added:",self.items)
+
+    def is_empty(self):
+        """Checks is list is empty"""
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
+
+    def dequeue(self):
+        """Deletes items from list"""
+        if self.is_empty():
+            return None
+        else:
+            self.items.pop(0)
+            print("Items left:",self.items)
+
+    def size(self):
+        """Checks for the size of the list"""
+        print("Length:", len(self.items))
+
+    def peek(self):
+        """Checks for single item in a list if list not is not empty"""
+        if self.is_empty():
+            return None
+        else:
+            print('Peek:', self.items[0])
+
+
+if __name__ == '__main__':
+    queue = Queue()
+    queue.enqueue(14)
+    queue.enqueue(5)
+    queue.enqueue(9)
+    queue.peek()
+    queue.size()
+    queue.dequeue()
+    queue.peek()
+    queue.dequeue()
+    queue.peek()
+    queue.dequeue()
+    queue.size()
+    queue.peek()
