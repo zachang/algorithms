c226ab69c466cb0be18d4a6891d6d91881fd63b0
zachang <zachangdawuda@gmail.com>
2018-07-23 10:33:33 +0100

    updated power_sum algorithm __doc__ and added executed entry point

--- power_digit_sum.py
+++ power_digit_sum.py
@@ -3,12 +3,15 @@ from functools import reduce
 
 
 def power_sum(power):
-    """Calculates sum of (2 to the power of n); n being a natural number <= 1000000"""
+    """
+    Calculates the resulting digits from sum of 2 to the power of n; 
+    n being a natural number <= 1000000
+    """
 
     value = str(2 ** power)
     value_list = list(value)
     _sum = reduce((lambda x, y: int(x) + int(y)), value_list)
     return _sum
 
-
-print(power_sum(1000))
+if __name__ == '__main__':
+    print(power_sum(15))
